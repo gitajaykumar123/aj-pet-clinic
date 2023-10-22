6dbfa4495cc8120f7d892c962b617779574e7fc4
Ajay Kumar <AjayKumar123@gmail.com>
2023-10-22 11:49:18 +0100

    Initial Set Up

---
+++ ajpetclinic/src/main/java/ajay/springframework/ajpetclinic/AjpetclinicApplication.java
@@ -0,0 +1,13 @@
+package ajay.springframework.ajpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjpetclinicApplication.class, args);
+    }
+
+}
